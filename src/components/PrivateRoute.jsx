354206c2232697a2637c1afe3c8e37c46cb77455
Raj Mohan <68589327+rajmohanreddy774@users.noreply.github.com>
2022-04-25 17:02:05 +0530

    Add files via upload

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import {Tempcontext} from './LogContext'
+import { useContext } from "react";
+import { createContext } from "react";
+import { Navigate } from 'react-router-dom';
+
+
+export function PrivateD({ children }) { 
+    const { isAuth, toggleAuth } = useContext(Tempcontext)   
+    if (!isAuth) {
+        return <Navigate to="/login" replace={false}/>
+    }
+    return children
+}
+
+
